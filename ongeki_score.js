4aa098ae12b9800dadfe0d4ebab4b7c6c1d47ebb
krpn06 <agepan.agepan@gmail.com>
2022-03-04 23:44:47 +0900

    Update ongeki_score.js

--- ongeki_score.js
+++ ongeki_score.js
@@ -1,6 +1,13 @@
 let wait = 500
+let master_num = 0
+let finish = 0
+let ptotal = 0
+var player = {name:"ＴＡＫＥＲＵＮ", s6:0, s5h:0, s5:0}
 var crawler_list = []
-var result_area_html = '<div style="background-color:rgb(200,200,255);border-radius:10px;"><div id="disp_result_area" style="padding:10px;"></div></div>'
+var music_ranking_master = []
+var music_ranking_master2 = []
+var total_p_score_ranking = []
+var result_area_html = '<div style="background-color:rgb(255,255,255);border-radius:10px;margin: 30px;padding: 10px;"><div id="disp_result_area"></div></div>'
 var level_list = [
     {id:1, name:"0"},
     {id:3, name:"1"},
@@ -30,16 +37,16 @@ var level_list = [
 ]
 
 function save_csv(data) {
-  let blob = new Blob([json2csv(data)], {type: 'text/csv'});
-  let url  = URL.createObjectURL(blob);
+    let blob = new Blob([json2csv(data)], {type: 'text/csv'});
+    let url  = URL.createObjectURL(blob);
 
-  let a = document.createElement("a");
+    let a = document.createElement("a");
 
-  a.href = url;
-  a.target = '_blank';
-  a.download = 'ongeki_score.csv';
+    a.href = url;
+    a.target = '_blank';
+    a.download = 'ongeki_score.csv';
 
-  a.click();
+    a.click();
 }
 
 function json2csv(json) {
@@ -51,170 +58,259 @@ function json2csv(json) {
         }).join(',');
     }).join("\n");
 
-    return header + body;
+    return body;
 }
 
 function loadScript(src, callback) {
-   let done = false;
-   let head = document.getElementsByTagName('head')[0];
-   let script = document.createElement('script');
-   script.src = src;
-   head.appendChild(script);
-   // Attach handlers for all browsers
-   script.onload = script.onreadystatechange = function() {
-       if ( !done && (!this.readyState ||
-               this.readyState === "loaded" || this.readyState === "complete") ) {
-           done = true;
-           callback();
-           // Handle memory leak in IE
-           script.onload = script.onreadystatechange = null;
-           if ( head && script.parentNode ) {
-               head.removeChild( script );
-           }
-       }
-   };
+    let done = false;
+    let head = document.getElementsByTagName('head')[0];
+    let script = document.createElement('script');
+    script.src = src;
+    head.appendChild(script);
+    // Attach handlers for all browsers
+    script.onload = script.onreadystatechange = function() {
+        if ( !done && (!this.readyState ||
+            this.readyState === "loaded" || this.readyState === "complete") ) {
+            done = true;
+            callback();
+            // Handle memory leak in IE
+            script.onload = script.onreadystatechange = null;
+            if ( head && script.parentNode ) {
+                head.removeChild( script );
+            }
+        }
+    };
 }
 function make_page_crawler(detail_crawler) {
-  let crawl_id = 0
-  let get_recursion = function() {
-    $.ajax({
-      type:"GET",
-      //contentType: "text/html; charset=EUC-JP",
-      url:"https://ongeki-net.com/ongeki-mobile/record/musicLevel/search/?level="+level_list[crawl_id].id,
-      dataType:"html"
-    }).done(response => {
-      let obj = $('.container3 form', response)
+    let crawl_id = 0;
+    let get_recursion = function() {
+        $.ajax({
+            type:"GET",
+            //contentType: "text/html; charset=EUC-JP",
+            url:"https://ongeki-net.com/ongeki-mobile/ranking/search/?genre=99&scoreType=5&rankingType=99&diff=3",
+            async: false,
+            dataType:"html"
+        }).done(response => {
+            let obj = $('.container3 form', response)
 
-        obj.each((index,element) => {
-          let difficult;
-          if ($('img:nth-child(2)', element)[0].src.match(/basic/)) {
-              difficult = "BASIC"
-          } else if ($('img:nth-child(2)', element)[0].src.match(/lunatic/)) {
-              difficult = "LUNATIC"
-          } else {
-              return // id取るだけならbasicだけで良い
-              if ($('img:nth-child(2)', element)[0].src.match(/advanced/)) {
-                  difficult = "ADVANCED"
-              }
-              if ($('img:nth-child(2)', element)[0].src.match(/expert/)) {
-                  difficult = "EXPERT"
-              }
-              if ($('img:nth-child(2)', element)[0].src.match(/master/)) {
-                  difficult = "MASTER"
-              }
-          }
-          //console.log($('.music_label', element).text())
-          const id = $('input', element).val()
-          const name = $('.music_label', element).text()
-          const level = level_list[crawl_id].name
-          let data = {
-              id: id,
-              difficult:difficult,
-              level:level,
-              name:name,
-          }
-          console.log(data)
-          crawler_list.push(data)
-        });
-        $("#disp_result_area").html("取得リストの作成中<br>"+crawl_id+"ページ目取得完了")
+            obj.each((index,element) => {
+                let difficult;
+                if ($('img:nth-child(2)', element)[0].src.match(/master/)) {
+                    difficult = "MASTER"
+                    master_num++
+                } else if ($('img:nth-child(2)', element)[0].src.match(/lunatic/)) {
+                    difficult = "LUNATIC"
+                }
+                //console.log($('.music_label', element).text())
+                const id = $('input[name="idx"]', element).val()
+                const name = $('.music_label', element).text()
+                const level = $('.score_level', element).text()
+                let data = {
+                    id: id,
+                    difficult:difficult,
+                    level:level,
+                    name:name,
+                }
+                console.log(data)
+                crawler_list.push(data)
+            });
+            $("#disp_result_area").html("取得リストの作成中<br>"+crawl_id+"ページ目取得完了")
 
-        crawl_id++
+            crawl_id++
 
-        if (crawl_id < level_list.length) {
-            //setTimeout(detail_crawler, wait, crawler_list)
-            setTimeout(get_recursion, wait)
-        } else {
-          setTimeout(detail_crawler, wait, crawler_list)
-      }
-    })
-  }
-  return get_recursion
+            if (crawl_id < 1) {
+                //setTimeout(detail_crawler, wait, crawler_list)
+                setTimeout(get_recursion, wait)
+            } else {
+                //save_csv(crawler_list)
+                setTimeout(detail_crawler, wait, crawler_list)
+            }
+        })
+
+        $.ajax({
+            type:"GET",
+            //contentType: "text/html; charset=EUC-JP",
+            url:"https://ongeki-net.com/ongeki-mobile/ranking/search/?genre=99&scoreType=5&rankingType=99&diff=10",
+            async: false,
+            dataType:"html"
+        }).done(response => {
+            let obj = $('.container3 form', response)
+
+            obj.each((index,element) => {
+                let difficult;
+                if ($('img:nth-child(2)', element)[0].src.match(/master/)) {
+                    difficult = "MASTER"
+                    master_num++
+                } else if ($('img:nth-child(2)', element)[0].src.match(/lunatic/)) {
+                    difficult = "LUNATIC"
+                }
+                //console.log($('.music_label', element).text())
+                const id = $('input[name="idx"]', element).val()
+                const name = $('.music_label', element).text()
+                const level = $('.score_level', element).text()
+                let data = {
+                    id: id,
+                    difficult:difficult,
+                    level:level,
+                    name:name,
+                }
+                console.log(data)
+                crawler_list.push(data)
+            });
+            $("#disp_result_area").html("取得リストの作成中<br>"+crawl_id+"ページ目取得完了")
+
+            crawl_id++
+
+            if (crawl_id < 1) {
+                //setTimeout(detail_crawler, wait, crawler_list)
+                setTimeout(get_recursion, wait)
+            } else {
+                //save_csv(crawler_list)
+                setTimeout(detail_crawler, wait, crawler_list)
+            }
+        })
+    }
+    return get_recursion
 }
 
 function make_crawler() {
-  let crawl_id = 0
-  let score_csv_data = []
+    let crawl_id = 0
 
-  var difficult_single_get = (music_data, selector_id, response) => {
-      var block_obj = $('#'+selector_id, response)
-      if (block_obj.length == 0) {
-          return
-      }
-      var last_play = $(".music_label table tbody tr:first-child td:nth-child(2)", block_obj).text()
-      var play_count = $(".music_label table tbody tr:nth-child(2) td:nth-child(2)", block_obj).text()
-      var over_damage_high_score = $(".score_table  tbody tr:nth-child(2) td:nth-child(1)", block_obj).text()
-      var battle_high_score = $(".score_table  tbody tr:nth-child(2) td:nth-child(2)", block_obj).text().replace(/,/g,"")
-      var technical_high_score = $(".score_table  tbody tr:nth-child(2) td:nth-child(3)", block_obj).text().replace(/,/g,"")
+    var music_ranking = []
+    get_recursion = function (crawler_list) {
+        //console.log(crawl_id)
+        $.ajax({
+            type: "GET",
+            url: "https://ongeki-net.com/ongeki-mobile/ranking/musicRankingDetail/?idx=" + crawler_list[crawl_id].id + "&scoreType=5&rankingType=99&diff=3",
+            data: {idx: crawler_list[crawl_id].id},
+            async: false,
+            //contentType: "text/html; charset=EUC-JP",
+            dataType: "html"
+        }).done(response => {
+            let obj = $('.border_block.master_score_back.m_5.p_5.t_l tr', response)
+            let title = $('.music_label.p_5.break', response).text()
+            let pmax = $('.t_r.p_5.f_18.f_b', response).text().replace(/.*\//, '').replace(/[^0-9]/g, '')
+            //alert(pmax)
+            let data1 = {
+                data: title,
+                pmax: Number(pmax)
+            }
+            music_ranking.push(data1)
 
-      var clear_flag = 0
-      if ($(".music_score_icon_area img:nth-child(1)", block_obj)[0].src.match(/music_icon_br_/)) {
-          clear_flag = 1
-      }
+            obj.each((index, element) => {
+                let player_name;
+                let platinum_score;
+                player_name = $('.t_l', element).text()
+                platinum_score = Number($('.f_18', element).text().replace(/,/, ''))
+                let data = {
+                    name: player_name,
+                    pscore: platinum_score,
+                    pmax: Number(pmax)
+                }
 
-      var bell_flag = 0
-      if ($(".music_score_icon_area img:nth-child(3)", block_obj)[0].src.match(/music_icon_fb.png/)) {
-          bell_flag = 1
-      }
+                music_ranking.push(data)
+            });
 
-      var ab_flag
-      if ($(".music_score_icon_area img:nth-child(4)", block_obj)[0].src.match(/music_icon_ab.png/)) {
-          ab_flag = "AB"
-      }
-      if ($(".music_score_icon_area img:nth-child(4)", block_obj)[0].src.match(/music_icon_fc.png/)) {
-          ab_flag = "FC"
-      }
+            crawl_id++
+            if(crawl_id > master_num) return
+            $("#disp_result_area").html("取得リストの作成中<br>" + crawl_id + "ページ目取得完了")
 
-      let data = {
-              music_id:music_data.id,
-              music_name:music_data.name,
-              difficulty:selector_id.toUpperCase(),
-              over_damage_high_score:over_damage_high_score,
-              battle_high_score:battle_high_score,
-              technical_high_score:technical_high_score,
-              play_count:play_count,
-              clear_flag:clear_flag,
-              bell_flag:bell_flag,
-              ab_flag:ab_flag,
-              last_play:last_play,
-      }
-      console.log(data)
-      score_csv_data.push(data)
-      $("#disp_result_area").html("music_id:"+music_data.id+"取得完了")
-      $("#disp_result_area").append(JSON.stringify(data)+"<br><br>");
-  }
-  let get_detail_data = (response, music_data) => {
-      difficult_single_get(music_data, "basic", response)
-      difficult_single_get(music_data, "advanced", response)
-      difficult_single_get(music_data, "expert", response)
-      difficult_single_get(music_data, "master", response)
-      difficult_single_get(music_data, "lunatic", response)
-   };
-  get_recursion = function(crawler_list) {
-    console.log(crawl_id)
-    $.ajax({
-      type:"GET",
-      url:"https://ongeki-net.com/ongeki-mobile/record/musicDetail",
-      data:{idx:crawler_list[crawl_id].id},
-      //contentType: "text/html; charset=EUC-JP",
-      dataType:"html"
-    }).done(response => {
-      get_detail_data(response,crawler_list[crawl_id])
-      crawl_id++
-      if (crawl_id < crawler_list.length) {
-        setTimeout(get_recursion, wait, crawler_list)
-        //save_csv(score_csv_data)
-      } else {
-        save_csv(score_csv_data)
-      }
-    })
-  }
-  return get_recursion
+                //if (crawl_id < master_num) {
+            if (crawl_id < master_num) {
+                setTimeout(get_recursion, 200, crawler_list)
+
+            } else {
+                finish++
+            }
+
+            /*
+            get_detail_data(response,crawler_list[crawl_id])
+            crawl_id++
+            if (crawl_id < crawler_list.length) {
+                setTimeout(get_recursion, wait, crawler_list)
+                //save_csv(score_csv_data)
+            } else {
+                //save_csv(score_csv_data)
+            }
+            */
+        })
+        if (finish === 1) {
+            finish++
+
+
+            let num = 0;
+
+            while (num < music_ranking.length) {
+                music_ranking_master.push(music_ranking[num])
+                num++
+            }
+            // ランキングから曲名を削除
+            num = 0
+            while (num < music_ranking_master.length) {
+                if (music_ranking_master[num].data) {
+                    ptotal += music_ranking_master[num].pmax
+                }
+                num++
+            }
+            console.log(music_ranking_master)
+            for (let i = 0; i < music_ranking_master.length; i++) {
+                if (music_ranking_master[i].data) {
+                    continue;
+                }
+                for (let j = 0; j < total_p_score_ranking.length; j++) {
+                    if (music_ranking_master[i].name === total_p_score_ranking[j].name) {
+                        total_p_score_ranking[j].pscore += music_ranking_master[i].pscore
+                        if (music_ranking_master[i].name === "ＴＡＫＥＲＵＮ") {
+                            let p = music_ranking_master[i].pscore / music_ranking_master[i].pmax
+                            if(p >= 0.99){
+                                player.s6++
+                                player.s5h++
+                                player.s5++
+                            } else if (p >= 0.98) {
+                                player.s5h++
+                                player.s5++
+                            } else if (p >= 0.97) {
+                                player.s5++
+                            }
+                        }
+                        break;
+                    }
+                }
+                total_p_score_ranking.push(music_ranking_master[i])
+            }
+
+            function compare(a, b) {
+                const numA = a.pscore;
+                const numB = b.pscore;
+
+                return (numA - numB) * -1;
+            }
+
+            total_p_score_ranking.sort(compare);
+
+
+            console.log(total_p_score_ranking)
+            save_csv(total_p_score_ranking)
+            //alert(ptotal)
+
+            $("#disp_result_area").html("")
+            $("#disp_result_area").append("PLATINUM SCORE RANKING<br>")
+            $("#disp_result_area").append("MAX : " + ptotal + "<br><br>")
+            $("#disp_result_area").append(player.name + "  ☆6(99%):" + player.s6 + "/" + master_num + " ☆5.5(98%):" + player.s5h + "/" + master_num + " ☆5(97%):" + player.s5 + "/" + master_num + "<br><br>")
+            for(let i = 0; i < 100; i++){
+                $("#disp_result_area").append(Number(i + 1) + ". " + total_p_score_ranking[i].name + " " + total_p_score_ranking[i].pscore + "<br>")
+            }
+
+            return
+        }
+
+    }
+    return get_recursion
 }
 
 loadScript("https://code.jquery.com/jquery-3.2.1.min.js", function() {
-  $(".wrapper").append(result_area_html);
-  detail_crawler = make_crawler()
-  page_crawler = make_page_crawler(detail_crawler)
-  page_crawler()
-
+    $(".wrapper").append(result_area_html);
+    detail_crawler = make_crawler()
+    page_crawler = make_page_crawler(detail_crawler)
+    page_crawler()
 })
